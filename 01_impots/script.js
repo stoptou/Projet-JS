47d9dabf48be1e63cad7543dd8efaa97714692e6
Eric_Astr <stoptou@gmail.com>
2021-12-12 14:28:18 +0100

    ajout du dossier reprographie

--- 01_impots/script.js
+++ 01_impots/script.js
@@ -1,21 +1,22 @@
 /* ---------------------------- Variables locale ---------------------------- */
-let age_input;
-let sexe_input;
+let age;
+let sexe;
 let Boutoncalc = document.getElementById("bouton_calcul");
 let affichage = document.getElementById("resultat");
 /* -------------------------------- Fonction -------------------------------- */
 function fc_calcul_impot() {
 
-    age_input = document.getElementById("age_input").value;
-    sexe_input = document.getElementById("sexe_input").value;
+    age = document.getElementById("age_input").value;
+    sexe = document.getElementById("sexe_input").value;
     // imposable si (Femme ET 35 >= age >= 18) OU (Homme ET age >=20)
-    if ((sexe_input == "F" && age_input >= 18 && age_input <= 35) || (sexe_input == "H" && age_input >= 20)) {
+    if ((sexe == "F" && age >= 18 && age <= 35) || (sexe == "H" && age >= 20)) {
         affichage.innerHTML =
             '<div class="alert alert-warning" role ="alert">Vous êtes imposable !</div>';
     } else {
         affichage.innerHTML =
             '<div class="alert alert-success" role ="alert">Vous êtes non imposable !</div>';
     }
+
 }
 // demarrage fonction sur click du "bouton_calcul"
 Boutoncalc.addEventListener("click", fc_calcul_impot, false);
---
+++ 02_Reprographie/script.js
@@ -0,0 +1,22 @@
+/* ---------------------------- Variables locale ---------------------------- */
+let nbcopies;
+let prixtotal;
+let Boutoncalc = document.getElementById("bouton_calcul");
+let affichage = document.getElementById("resultat");
+/* -------------------------------- Fonction -------------------------------- */
+function fc_calcul() {
+    nbcopies = document.getElementById("nb_input").value;
+    if (nbcopies <= 10) {
+        prixtotal = (nbcopies) * 0.1;
+    } else {
+        if (nbcopies <= 30) {
+            prixtotal = (nbcopies - 10) * 0.09 + 1;
+        } else {
+            prixtotal = (nbcopies - 30) * 0.08 + 2.8;
+        }
+    }
+    affichage.innerHTML =
+        `<div class="alert alert-success" role ="alert">${prixtotal} €</div>`;
+}
+/* ---------------------------------- Appel --------------------------------- */
+Boutoncalc.addEventListener("click", fc_calcul, false);
