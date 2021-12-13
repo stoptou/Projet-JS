ad79e7ea98e81c5390a3d0bac41e192fb179efa7
Eric_Astr <stoptou@gmail.com>
2021-12-13 15:23:59 +0100

    Ajout de la fonction "strip" pour éviter les trop longues décimales.

--- 02_Reprographie/script.js
+++ 02_Reprographie/script.js
@@ -14,10 +14,16 @@ function fc_calcul() {
         } else {
             prixtotal = (nbcopies - 30) * 0.08 + 2.8;
         }
+        prixtotal = strip(prixtotal);
     }
     affichage.innerHTML =
         `<div class="alert alert-success" role ="alert">${prixtotal} €</div>`;
 }
-
+/* ----------------------------- Fonction strip ----------------------------- */
+// Parfois on se retrouve avec de très nombreuses décimales (ex: 2.34999999999 au lieu de  2.35)
+// cette fonction résoud le problème
+function strip(number) {
+    return (parseFloat(number).toPrecision(6));
+}
 /* ---------------------------------- Appel --------------------------------- */
 Boutoncalc.addEventListener("click", fc_calcul, false);
